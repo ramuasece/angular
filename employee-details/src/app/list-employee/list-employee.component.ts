ee31183a82c8262412dfce35cde742b911337487
ramu <ramu.garkini@gmail.com>
2020-05-29 11:49:12 +0530

    creates CRUD Employee details in Angular

---
+++ employee-details/src/app/list-employee/list-employee.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeDetailsService } from '../employee-details.service';
+
+@Component({
+  selector: 'app-list-employee',
+  templateUrl: './list-employee.component.html',
+  styleUrls: ['./list-employee.component.css']
+})
+export class ListEmployeeComponent implements OnInit {
+
+  constructor(private employeeDetails:EmployeeDetailsService) { }
+  collection={};
+
+  ngOnInit(): void {
+    this.employeeDetails.getList().subscribe((result)=>{
+      // console.warn(result)
+      this.collection=result
+    })
+  }
+
+  deleteEmployeeDetails(item)
+  {
+    // console.warn(item)
+    this.collection.splice(item-1, 1)
+
+    this.employeeDetails.deleteEmployeeDetails(item).subscribe((result)=>{
+      console.warn(result)
+    })
+  }
+}
